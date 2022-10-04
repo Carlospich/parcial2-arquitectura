1acc7aa22b866f23abadb336d178e523f2337d45
Carlospich <carlospich@unisabana.edu.co>
2022-10-04 08:57:35 -0500

    Implementación Jacoco, Pruebas Unitarias

--- src/test/java/co/edu/unisabana/parcialarquitectura/service/BuyTest.java
+++ src/test/java/co/edu/unisabana/parcialarquitectura/service/BuyTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class BuyTest {
 
     private final Buy service = new Buy(new Database());
@@ -19,7 +22,18 @@ class BuyTest {
     }
 
     @Test
-    public void Given_codes_When_buyer_code_and_vendor_code_equals_Then_verify_throw_exception() {
+    public void Given_codes_When_buyer_code_and_vendor_code_equals_Then_verify_throw_Illegal_exception() {
+        assertThrows(IllegalSaleException.class, ()-> service.makePurchase(3, 3, "Books"));
 
     }
+    @Test
+    public void Given_vendor_and_different_buyer_When_purchase_Then_sold() {
+        service.makePurchase(2, 3, "Books");
+        assertEquals("Product sold", service.makePurchase(2, 3, "Books"));
+    }
+    @Test
+    public void Given_vendor_and_different_buyer_with_null_item_When_purchase_Then_sale_couldnt_be_possible() {
+        assertEquals("The sale was not possible", service.makePurchase(2, 3, null));
+    }
+
 }
